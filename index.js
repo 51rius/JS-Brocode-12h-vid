c3858007b18f7c4c4960be22294faebd64c1a354
Juliano Assunção Machado <juliano51.dev@proton.me>
2025-06-01 02:12:48 -0300

    V1.52.3
    Algoritmo de input que mostra na tela o que foi escrito depois de clicar no botão.

---
+++ index.js
@@ -0,0 +1,12 @@
+document.getElementById('laele').addEventListener('click', ()=> {
+    document.getElementById('textaço').textContent = document.getElementById('entrou').value
+})
+
+// let textaço = document.getElementById('textaço')
+// let botao = document.getElementById('laele')
+// let entrada = document.getElementById('entrou')
+
+// botao.onclick = ()=>{
+//     textaço.textContent = entrada.value
+//     entrada.value = ''
+// }
